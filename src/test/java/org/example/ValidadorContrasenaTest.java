ed572afa32a73e237d584721b22c588995042cf9
Daniel Martiñán <danielmartinan@edu.xunta.gal>
2025-02-20 19:20:31 +0100

    Creados tests de validacion de validarContrasena

--- src/test/java/org/example/ValidadorContrasenaTest.java
+++ src/test/java/org/example/ValidadorContrasenaTest.java
@@ -6,5 +6,45 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidadorContrasenaTest {
+    ValidadorContrasena validador;
 
+    @BeforeEach
+    void setUp() {
+        validador = new ValidadorContrasena();
+    }
+
+    @Test
+    void testValidarContrasenaNula() {
+        assertFalse(validador.validar(null));
+    }
+
+    @Test
+    void testValidarContrasenaCorta(){
+        assertFalse(validador.validar("abcd"));
+    }
+
+    @Test
+    void testValidarContrasenaMinusculas(){
+        assertFalse(validador.validar("contrasena"));
+    }
+
+    @Test
+    void testValidarContrasenaMinusYMayus(){
+        assertFalse(validador.validar("Contrasena"));
+    }
+
+    @Test
+    void testValidarContrasenaMinusMayusYNumero() {
+        assertFalse(validador.validar("Contrasena1"));
+    }
+
+    @Test
+    void testValidarContrasenaSinMinuscula() {
+        assertFalse(validador.validar("CONTRASENA1!"));
+    }
+
+    @Test
+    void testValidarContrasenaValida() {
+        assertTrue(validador.validar("Contrasena1!"));
+    }
 }
